c69aae1da4383075198990d753b7a06c86d0d442
Brad Severance <me@bradseverance.com>
2019-08-11 14:00:07 -0400

    prod stuff; img path

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <div class="flex-center position-ref full-height">
 	<div class="content">
+		<img id="book-img" alt="Brad's Books" src="/imgs/book.png" />
 		<h4>You're logged in! (Not really, but kinda)</h4>
 		<div class="title m-b-md">Administrate All the Books!</div>
 		<h3>Do stuff like add a book, edit a book, delete a book, and so forth and so on ...</h3>
--- resources/views/other/welcome.blade.php
+++ resources/views/other/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="flex-center position-ref full-height">
 	<div class="content">
-		<img id="book-img" alt="Brad's Books" src="/imgs/book.png" />
+		<img id="book-img" alt="Brad's Books" src="/public/imgs/book.png" />
 		<div class="title m-b-md">Brad's Books</div>
 		<h3>I don't want to make any money, folks ... I just love to sell books.</h3>
 	</div>
