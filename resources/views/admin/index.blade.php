f3668b36878917f8db51162ff1abbaee273cbd7c
Brad Severance <me@bradseverance.com>
2019-08-11 14:15:41 -0400

    finishing up

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="flex-center position-ref full-height">
 	<div class="content">
-		<img id="book-img" alt="Brad's Books" src="/public/imgs/book.png" />
+		<img id="book-img" class="add-bottom" alt="Brad's Books" src="/public/imgs/book.png" />
 		<h4>You're logged in! (Not really, but kinda)</h4>
 		<div class="title m-b-md">Administrate All the Books!</div>
 		<h3>Do stuff like add a book, edit a book, delete a book, and so forth and so on ...</h3>
